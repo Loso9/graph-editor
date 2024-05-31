4b46a5d82257bd6bf0a22fa5e49678d1eed5bfc1
Loso9 <92187237+Loso9@users.noreply.github.com>
2024-05-31 23:52:21 +0200

    adding documentation

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.editorgrafov","l":"com.example.editorgrafov"},{"m":"com.example.editorgrafov","l":"com.example.editorgrafov.enums"},{"m":"com.example.editorgrafov","l":"com.example.editorgrafov.tuples"}];updateSearchResults();
